b0380e9b1980c904853219c5b44dc1a57d14b04f
Preeti Rawat <preetirawat711@gmail.com>
2021-06-04 14:11:53 +0530

    New Project. JavaFX

---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module HelloWordFX {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,8 +2,15 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +18,16 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    //        GridPane root=new GridPane();
+    //        root.setAlignment(Pos.CENTER);
+    //        root.setHgap(10);
+    //        root.setVgap(10);
+    //        Label greetings=new Label("Welcome to JavaFX..");
+    //        greetings.setTextFill(Color.BLUE);
+    //        greetings.setFont(Font.font("Times New Roman", FontWeight.BOLD,30));
+    //        root.getChildren().add(greetings);
+        primaryStage.setTitle("Hello JavaFX!");
+        primaryStage.setScene(new Scene(root, 500, 275));
         primaryStage.show();
     }
 
